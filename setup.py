7abb113a45eca52ec7599391aaa89fa72714a2a0
RileyTheEcon <r.conlon9494@gmail.com>
2026-03-04 12:55:55 -0800

    Adding necessary library files

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 12:49:14 2026
+
+@author: RC
+"""
+
+
+
+
+
+# ================================== SETUP ================================== #
+from setuptools import setup, find_packages
+
+setup(
+    name="Riley's Library",
+    version="0.0.1",
+    description=("This is a repository of all the snippets that I develop "+
+                 "that don't fit into a single project but I still use frequently."
+                 ),
+    #packages=find_packages(),
+    #install_requires=[] # List dependencies here
+)
+
+# =========================================================================== #
+
+
+
+
+
+
+
+
